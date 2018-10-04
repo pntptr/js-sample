a8efef0d362fa03ffe0a48c0d623bb952ee20bae
pietro <pietro.pnt@gmail.com>
2018-10-04 16:58:13 +0200

    aggiunto oggetto popolato con attributi e metodi

--- s3/l9.js
+++ s3/l9.js
@@ -9,3 +9,24 @@ console.log(test + ' => ' + typeof test);
 
 test = 42;
 console.log(test + ' => ' + typeof test);
+
+// array
+test = [4, 5, 6];
+console.log(test + ' => ' + typeof test);
+
+// object
+test = {};
+console.log(test + ' => ' + typeof test);
+
+// object initialized
+person = {
+    name : 'Alfa',
+    lastname : 'Romeo',
+    sayHello : () => {
+        console.log('Hi all!');
+    }
+};
+
+person.sayHello();
+console.log(person.name + ' ' + person.lastname + ' => ' + typeof person);
+
