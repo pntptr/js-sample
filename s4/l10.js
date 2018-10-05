ad4c3a614cd3c8de7dc40e08b5586cdf22c73641
pietro <pietro.pnt@gmail.com>
2018-10-05 12:31:20 +0200

    operazioni di concatenazione tra stringhe

--- s4/l10.js
+++ s4/l10.js
@@ -4,7 +4,15 @@
 var nome = "Nome";
 var cognome = 'Cognome';
 var indirizzo = "Via \"da qui\"";
+var indirizzo2 = 'Via "larga"';
+
+// nota in UTF_8
+var nota_utf8 = '\u0045';
 
 console.log(nome);
 console.log(cognome);
 console.log(indirizzo);
+console.log(indirizzo2);
+
+// stampa della nota in UTF_8
+console.log(nota_utf8);
---
+++ s4/l11.js
@@ -0,0 +1,6 @@
+// SEZIONE 4, LEZIONE 11 - CORSO UDEMY JS
+// operazioni di concatenazione sulle stringhe
+
+var verso1 = "Ahi quanto a dir qual era è cosa dura,\n";
+var verso2 = "esta selva selvaggia e aspra e forte\n";
+var verso3 = "che nel pensier rinova la paura!\n";
