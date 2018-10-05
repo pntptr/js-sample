2d233aa4004c6d8ae74067ed936f89b5a9eac840
pietro <pietro.pnt@gmail.com>
2018-10-05 12:14:31 +0200

    uso delle variabili di tipo string

---
+++ s4/l10.js
@@ -0,0 +1,10 @@
+// SEZIONE 4, LEZIONE 10 - CORSO UDEMY JS
+// variabili di tipo string
+
+var nome = "Nome";
+var cognome = 'Cognome';
+var indirizzo = "Via \"da qui\"";
+
+console.log(nome);
+console.log(cognome);
+console.log(indirizzo);
