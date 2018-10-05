adb2c1c83d3f34685a4a90889b611a446be7e9a3
pietro <pietro.pnt@gmail.com>
2018-10-05 16:54:22 +0200

    wrap dei primitivi

--- s5/l15.js
+++ s5/l15.js
@@ -1,4 +1,26 @@
 // SEZIONE 5, LEZIONE 13 - CORSO UDEMY JS
 // oggetto number
 
+var num = new Number("2");
+console.log(num);
+console.log(typeof num);
+console.log(num.valueOf());
 
+// test per il valore nell'oggetto num
+console.log(num == 2); // num vale 2 quindi è true
+console.log(num === 2); // num è un oggetto quindi è diverso dal tipo primitivo quindi false
+
+// casting
+var eta = "32";
+console.log(eta + " => " + typeof(eta));
+eta = +eta;
+console.log(eta + " => " + typeof(eta));
+eta = parseInt(eta);
+console.log(eta + " => " + typeof(eta));
+
+// Javascript utilizza il wrapper Number per lavorare con i numeri
+var wrapInt = (2).toString(); // wrap di un primitivo in un oggetto tipo Number
+console.log(wrapInt);
+
+wrapInt = (2).toFixed(2);
+console.log(wrapInt);
