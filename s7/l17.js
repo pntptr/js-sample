e2348b131ba87ed5ac9478fda6b05257bd238780
pietro <pietro.pnt@gmail.com>
2018-10-06 17:48:30 +0200

    tipo primitivo boolean cast e wrap

--- s7/l17.js
+++ s7/l17.js
@@ -1,2 +1,30 @@
 // SEZIONE 7, LEZIONE 17 - CORSO UDEMY JS
 // tipo boolean
+
+var mioValore = false;
+var secondoValore = true;
+console.log(mioValore);
+console.log(secondoValore);
+
+// wrap di un valore booleano
+var bool = Boolean(6 > 7);
+console.log(bool);
+
+var boolObj = new Boolean("Ciao");
+console.log(boolObj);
+
+// cast di un booleano
+var toCast = 12;
+toCast = !!toCast;
+console.log("Valore di toCast: " + toCast + " tipo: " + typeof(toCast));
+
+// i seguenti valori sono sempre false: 
+// false, null, undefined, 0, NaN, stringa vuota ""
+//
+// mentre tutti gli altri sono sempre true
+// persino new Boolean(false) perchè è un oggetto
+
+var test = new Boolean(0);
+console.log("Valore di test con 0: " + test);
+test = new Boolean(1);
+console.log("Valore di test con 1: " + test);
