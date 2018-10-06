e31baf107d22a2ec49ae09b396e418f65276886d
pietro <pietro.pnt@gmail.com>
2018-10-06 18:05:05 +0200

    operazioni sugli array

--- s8/l18.js
+++ s8/l18.js
@@ -2,3 +2,10 @@
 // array - dichiarazione con costruttore new Array()
 
 var giorni = new Array();
+console.log(giorni);
+giorni = new Array(7);
+console.log(giorni);
+
+// Javascript effettua un fill delle posizioni rimanenti con undefined: l'array cresce!
+giorni[10] = "Test";
+console.log(giorni);
