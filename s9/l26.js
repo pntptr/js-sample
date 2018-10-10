7a7cc810694f0c2713a06ac0c2557a94de05c77a
pietro <pietro.pnt@gmail.com>
2018-10-10 15:32:52 +0200

    aggiunta lezione sugli operatori di incremento decremento

--- s9/l26.js
+++ s9/l26.js
@@ -1,3 +1,23 @@
 // SEZIONE 9, LEZIONE 26 - CORSO UDEMY JS
 // operatori matematici unari ++ e --
 
+var y = 0;
+var num = 5;
+
+// operatore ++
+y = num++;
+console.log(y);
+console.log(num);
+
+y = ++num;
+console.log(y);
+console.log(num);
+
+// operatore --
+y = num--;
+console.log(y);
+console.log(num);
+
+y = --num;
+console.log(y);
+console.log(num);
