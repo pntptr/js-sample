ae9ab0bb7a176a5e7af91165f835afe807920709
pietro <pietro.pnt@gmail.com>
2018-10-10 12:22:00 +0200

    conclusa lezione sugli operatori matematici

--- s9/l23.js
+++ s9/l23.js
@@ -15,3 +15,32 @@ console.log(z);
 y = 12.5;
 z = x + y;
 console.log(z); 
+
+z = x * y;
+console.log(z);
+
+z = y / x;
+console.log(z);
+
+z = 100 % 20;
+console.log(z);
+
+z = 100 % 22;
+console.log(z);
+
+// operazioni particolari
+z = 100/0;
+console.log(z);
+console.log(typeof(z));
+
+z = 23 * "Test";
+console.log(z);
+
+z = 23 + "Test";
+console.log(z);
+z = parseInt(z);
+console.log(z);
+z = 23 + parseInt("Test");
+console.log(z);
+// in caso di operazioni numeriche è sempre meglio effettuare un
+// parse per evitare errori o valori inaspettati
