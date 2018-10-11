1a0498808039c3be3d4dfeb305cfe24cfc8217c5
pietro <pietro.pnt@gmail.com>
2018-10-11 13:30:27 +0200

    confronti per valore e tipo

--- s9/l28.js
+++ s9/l28.js
@@ -7,3 +7,29 @@ var num2 = '12';
 
 var test = num == num2; // cast implicito della stringa a numero
 console.log(test);
+
+test = num === num2;
+console.log(test);
+
+// altri confronti critici
+var numero1 = 1;
+var vero = true;
+var falso = false;
+
+test = numero1 == vero; // cast implicito del booleano da true a 1
+console.log(test);
+console.log(true * 5);
+
+test = numero1 === vero;
+console.log(test);
+
+test = falso == 0;
+console.log(test);
+test = falso === 0;
+console.log(test);
+
+console.log(vero == falso);
+console.log(vero != falso);
+
+console.log(vero != 1);
+console.log(vero !== 1);
