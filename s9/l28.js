9cd34a3140e16cdee8b751ab8c9572eebdfe4b5e
pietro <pietro.pnt@gmail.com>
2018-10-11 13:11:41 +0200

    aggiunta lezione sui confronti di tipo

---
+++ s9/l28.js
@@ -0,0 +1,9 @@
+// SEZIONE 9, LEZIONE 28 - CORSO UDEMY JS
+// operatori matematici confronto
+
+// operatori !=, ==, !==, ===
+var num = 12;
+var num2 = '12';
+
+var test = num == num2; // cast implicito della stringa a numero
+console.log(test);
