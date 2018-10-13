d4befe48a65938f952032006d162098b4e0941e9
pietro <pietro.pnt@gmail.com>
2018-10-13 15:09:29 +0200

    terminata lezione su operatore NOT

--- s9/l32.js
+++ s9/l32.js
@@ -1,2 +1,9 @@
 // SEZIONE 9, LEZIONE 32 - CORSO UDEMY JS
 // operatore logico NOT (!)
+
+var result = 0;
+console.log(Boolean(result));
+result = !0;
+console.log(Boolean(result));
+result = !"test";
+console.log(Boolean(result));
