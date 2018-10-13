a1c767f9fb6cefea3efd4d1455a2060d860f12a9
pietro <pietro.pnt@gmail.com>
2018-10-13 14:56:17 +0200

    operatore OR

--- s9/l31.js
+++ s9/l31.js
@@ -1,2 +1,8 @@
 // SEZIONE 9, LEZIONE 31 - CORSO UDEMY JS
 // operatore logico OR (||)
+
+var test = 1 || 0; // se il primo valore è true, restituisce subito il primo
+console.log(test);
+
+test = 0 || 2;
+console.log(test);
