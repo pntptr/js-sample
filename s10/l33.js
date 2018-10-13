312e78c460b12a9c959e9113e67467ab59f2c0cc
pietro <pietro.pnt@gmail.com>
2018-10-13 15:24:59 +0200

    istruzioni condizionali

--- s10/l33.js
+++ s10/l33.js
@@ -1,2 +1,40 @@
 // SEZIONE 10, LEZIONE 33 - CORSO UDEMY JS
 // introduzione a istruzioni condizionali e cicli
+
+// istruzioni condizionali in Javascript
+var valore;
+
+if(valore){
+    // do something...
+}
+
+if(valore){
+    // do something...
+} else {
+    // do something else...
+}
+
+if(valore){
+    // do something...
+} else if(valore) {
+    // do something...
+} else {
+    // do something else...
+}
+
+switch(valore){
+    case valore:{
+        // do something...
+    }
+    default:{
+        // do something...
+    }
+}
+
+while(valore){
+    // do something...
+}
+
+do{
+    // do something...
+} while(valore);
