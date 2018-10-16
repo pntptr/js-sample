c3f8a7d2ef3a461a424ad9e2f20e8f4fd973c8c6
pietro <pietro.pnt@gmail.com>
2018-10-16 14:19:55 +0200

    uso del ciclo for

---
+++ s11/l39.js
@@ -0,0 +1,23 @@
+// SEZIONE 10, LEZIONE 39 - CORSO UDEMY JS
+// ciclo for
+var maxLoop = 10;
+
+for(var i = 0; i < maxLoop; i++){
+    console.log(i);
+}
+
+// dichiarazione alternativa del ciclo for
+var j = 0;
+
+for(; j < maxLoop;){
+    console.log(j++);
+}
+
+// parole chiave per i cicli
+for(var k = 0; k < maxLoop; k++){
+    if(k == 5) continue; // salta il valore 5 dalla numerazione
+    if(k == 8) break;   // esce dal ciclo appena il valore è 8
+    console.log(k);
+}
+
+console.log('End of program...');
