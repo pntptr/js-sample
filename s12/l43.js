9ec09b7edf9abf6ef6beb439c62d6c3ecc7eddd6
pntptr <pietro.pnt@gmail.com>
2018-10-23 23:15:18 +0200

    aggiunta implementazione a funzione hoursToSeconds

--- s12/l43.js
+++ s12/l43.js
@@ -1,3 +1,13 @@
 // SEZIONE 12, LEZIONE 43 - CORSO UDEMY JS
 // funzioni con argomenti
 
+function hoursToSeconds(hours){
+    // effetuo un cast di hours a numerico, se non va a buon fine restituisco lo 0
+    var h = +hours || 0; 
+    return h * 3600;
+}
+
+console.log(hoursToSeconds(24));
+console.log(hoursToSeconds(2));
+console.log(hoursToSeconds('2'));
+console.log(hoursToSeconds(undefined));
