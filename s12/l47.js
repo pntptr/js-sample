c63b7ee34716610ac3f88c5e99da0a5dd57c4c9a
pntptr <pietro.pnt@gmail.com>
2018-11-05 22:16:41 +0100

    completata lezione sullo scope

--- s12/l47.js
+++ s12/l47.js
@@ -1,3 +1,13 @@
 // SEZIONE 12, LEZIONE 47 - CORSO UDEMY JS
 // scope delle variabili
 
+// scope globale
+var myVar = 'Primo';
+
+function testVar(){
+    var myVar = 'Seconda';
+    console.log(myVar);
+}
+
+testVar();
+console.log(myVar);
