7537773f7948094a2d3013494a7f429fb080fbb6
pietro <pietro.pnt@gmail.com>
2018-11-24 13:32:41 +0100

    creata funzione anonima per oggetto callback

--- s12/l51.js
+++ s12/l51.js
@@ -4,13 +4,18 @@
 // funzione di output di un oggetto
 function outputObject(obj){
     for(var i in obj){
-        console.log(obj[i]);
+        console.log(i + ' => ' + obj[i]);
     } 
 }
 
 // funzione di elaborazione di un oggetto: passo l'oggetto ed un metodo da eseguire su di esso
 function processObject(myObj, callback){
     callback(myObj);
+
+    // creo una funzione anonima per l'oggetto callback
+    callback.testFunction = function() {
+        console.log('Called testFunction()');
+    }
 }
 
 // creo l'oggetto
@@ -18,3 +23,6 @@ var persona = {name:'Severus', age:'50', genre:'M'};
 
 // processo l'oggetto appena creato
 processObject(persona, outputObject);
+
+// l'oggetto callback si chiama outputObject(), quindi posso usare la funzione testFunction() 
+outputObject.testFunction();
