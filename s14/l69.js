7a49d62a4777274ed52a0758e15b6095e59fe4a1
pietro <pietro.pnt@gmail.com>
2018-12-25 10:55:32 +0100

    aggiunta lezione su reduce()

--- s14/l69.js
+++ s14/l69.js
@@ -1,3 +1,17 @@
 // SEZIONE 14, LEZIONE 69 - CORSO UDEMY JS
 // METODI DELL'OGGETTO ARRAY IN JAVASCRIPT
 // Il metodo reduce(): 
+
+var numbers = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9
+];
+
+// REMOVE THE LAST ELEMENTS FOR TESTING
+// numbers.splice(4);
+
+// REDUCE METHOS: RETURNS A VALUE FROM A CALLBACK
+var sum = numbers.reduce(
+    (prevValue, nextValue) => prevValue + nextValue
+);
+
+console.log('Sum: ' + sum);
