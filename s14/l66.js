3ebe2a1ca10ac470e55a73285ca6183b19cb5249
pietro <pietro.pnt@gmail.com>
2018-12-25 10:45:22 +0100

    aggiunte lezioni da 66 a 69, da implementare

---
+++ s14/l66.js
@@ -0,0 +1,3 @@
+// SEZIONE 14, LEZIONE 66 - CORSO UDEMY JS
+// METODI DELL'OGGETTO ARRAY IN JAVASCRIPT
+// Il metodo filter(): filtra gli elementi in un array ed esegue una callback (funzione)
---
+++ s14/l67.js
@@ -0,0 +1,3 @@
+// SEZIONE 14, LEZIONE 67 - CORSO UDEMY JS
+// METODI DELL'OGGETTO ARRAY IN JAVASCRIPT
+// Il metodo forEach(): esegue una funzione di callback per ogni elemento presente nell'array
