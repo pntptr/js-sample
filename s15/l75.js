f4d53e96fc25740fbb0f9f13ff41a8acd9b5797f
pietro <pietro.pnt@gmail.com>
2018-12-28 01:26:29 +0100

    aggiunta lezione 75 su parola chiave this

---
+++ s15/l75.js
@@ -0,0 +1,37 @@
+// SEZIONE 15, LEZIONE 74 - CORSO UDEMY JS
+// OBJECT ORIENTED PROGRAMMING. OGGETTI E CLASSI IN JAVASCRIPT
+// SCOPE NELLE FUNZIONI, PAROLA CHIAVE THIS, AMBIENTE DI ESECUZIONE
+
+// DEFINE A LITERAL OBJECT
+var dog = {
+    type : 'DOG',
+    numLegs : 4,
+    name : 'Goofye',
+
+    bark : function(){
+        console.log('WOOF!');
+    },
+
+    getName : function(){
+        console.log(this.name);
+    },
+
+    favoriteFriends : [],
+
+    id : {
+        idNumber : 123456,
+        address : 'Via G. La Pira, Persico Dosimo',
+        country : 'ITALY'
+    },
+
+    // CREATE A METHOD WITH EXTERNAL DEFINITION (REFERENCES AN EXTERNAL FUNCTION)
+    displayDogId : displayId 
+
+}
+
+// CREATE A FUNCTION THAT SHOWS AN OBJECT ID
+function displayId(){
+    console.log(this.id);
+}
+
+dog.displayDogId();
