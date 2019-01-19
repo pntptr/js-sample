769ed4b8eef2c49a2bdc00aa074ec891280a94fe
pietro <pietro.pnt@gmail.com>
2019-01-19 16:39:05 +0100

    terminata lezione 82

--- s17/l82/l82.js
+++ s17/l82/l82.js
@@ -2,4 +2,27 @@
 // JAVASCRIPT LATO CLIENT: MANIPOLARE IL DOM
 // funzione getElementsByTagName
 
+// adding an event listener when the page is loaded
+document.addEventListener('DOMContentLoaded', function(colorChanged){ 
+    // access directly to a background property of the body
+    //document.body.style.backgroundColor = 'white';
+    
+    // getting dom element by getElementsByTagName function
+    var e = document.getElementsByTagName('body');
+    var a = ['a', 'a', 'a'];
 
+    a.forEach(elem => {
+        console.log(elem);
+    });
+
+    console.log(e);
+
+    for(var i = 0; i < e.length; i++){
+        e[i].style.color = 'green';
+    }
+
+    // tables
+    var t = document.getElementsByTagName('tables');
+    console.log(t);
+    
+});
