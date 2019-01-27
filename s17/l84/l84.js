e5de746890b3adabdd87f3837b2b2ba9e659baa6
pietro <pietro.pnt@gmail.com>
2019-01-27 18:04:43 +0100

    aggiunta lezione 84 su QUERYSELECTOR

--- s17/l84/l84.js
+++ s17/l84/l84.js
@@ -1,3 +1,8 @@
 // SEZIONE 17, LEZIONE 84 - CORSO UDEMY JS
 // JAVASCRIPT LATO CLIENT: MANIPOLARE IL DOM
 // querySelector, querySelectorAll
+
+document.addEventListener('DOMContentLoaded', function(){
+    var h1 = document.querySelector('#title');
+    h1.style.color = "green";
+});
