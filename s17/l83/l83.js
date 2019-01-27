3f08e6d3d5034cbdb18889ca0b23259613ba5a29
pietro <pietro.pnt@gmail.com>
2019-01-27 16:38:33 +0100

    aggiunta lezione 83

---
+++ s17/l83/l83.js
@@ -0,0 +1,8 @@
+// SEZIONE 17, LEZIONE 83 - CORSO UDEMY JS
+// JAVASCRIPT LATO CLIENT: MANIPOLARE IL DOM
+// getElementById and getElementByClassName
+
+document.addEventListener('DOMContentLoaded', function() {
+    // listener 
+    var h1 = document.getElementById('title');
+});
