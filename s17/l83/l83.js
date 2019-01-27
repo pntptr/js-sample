ce82b66a613802632476f947552b01aac01689d3
pietro <pietro.pnt@gmail.com>
2019-01-27 16:57:06 +0100

    aggiunti listener di eventi alla pagina

--- s17/l83/l83.js
+++ s17/l83/l83.js
@@ -5,4 +5,19 @@
 document.addEventListener('DOMContentLoaded', function() {
     // listener 
     var h1 = document.getElementById('title');
+    h1.style.textTransform = 'uppercase';
+    h1.style.color = 'green';
+    h1.innerHTML = 'hello Daniele!';
+
+    h1.onclick = () => {
+        alert('You clicked me! Daniele is stupid!');
+    }
+
+    h1.addEventListener('mouseover', function () {
+        h1.style.color = 'red';
+    });
+    
+    h1.addEventListener('mouseleave', function () {
+        h1.style.color = 'green';
+    });
 });
