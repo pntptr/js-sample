924b649a2aaa7caaa4655b87744f83a04faa4d28
pietro <pietro.pnt@gmail.com>
2019-02-02 15:38:02 +0100

    aggiunta lezione 85

---
+++ s17/l85/l84.js
@@ -0,0 +1,8 @@
+// SEZIONE 17, LEZIONE 85 - CORSO UDEMY JS
+// JAVASCRIPT LATO CLIENT: MANIPOLARE IL DOM
+// querySelectorAll
+
+document.addEventListener('DOMContentLoaded', function(){
+    var h1 = document.querySelector('#title'); // querySelector() returns only one element
+    h1.style.color = "green";
+});
