ad1bb2932579c6a4751741a1a71b94f02b6caeae
pietro <pietro.pnt@gmail.com>
2019-02-02 16:13:41 +0100

    aggiunto selector per tutti i paragrafi

--- s17/l85/l85.js
+++ s17/l85/l85.js
@@ -5,4 +5,10 @@
 document.addEventListener('DOMContentLoaded', function(){
     var h1 = document.querySelector('#title'); // querySelector() returns only one element
     h1.style.color = "green";
+
+    // usage of querySelectorAll()
+    var p = document.querySelectorAll('p');
+    p.forEach((target) => {
+        target.style.color = "green";
+    });
 });
