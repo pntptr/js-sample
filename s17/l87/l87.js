87e505bd56a17132b881d5782c59d2cb9c371157
pietro <pietro.pnt@gmail.com>
2019-02-02 18:02:37 +0100

    creato array di siti da caricare nel DOM

--- s17/l87/l87.js
+++ s17/l87/l87.js
@@ -1,7 +1,31 @@
 // SEZIONE 17, LEZIONE 87 - CORSO UDEMY JS
 // JAVASCRIPT LATO CLIENT: MANIPOLARE IL DOM
-// 
+// createElement e createTextNode
 
+var menu = [
+    {
+        url: 'http://www.google.it',
+        title: 'Google - Home Page'
+    },
+    {
+        url: 'http://www.google.it',
+        title: 'Google - Home Page'
+    },
+    {
+        url: 'http://www.google.it',
+        title: 'Google - Home Page'
+    },
+    {
+        url: 'http://www.google.it',
+        title: 'Google - Home Page'
+    },
+    {
+        url: 'http://www.google.it',
+        title: 'Google - Home Page'
+    }
+];
+
+// execute callback when the DOM is loaded
 document.addEventListener('DOMContentLoaded', function(){
     
 });
