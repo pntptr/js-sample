4e561e1d9661a4c8ca22544044b655993147dbca
pietro <pietro.pnt@gmail.com>
2019-02-16 16:59:52 +0100

    metodi alternativi per aggiungere gestore di eventi

--- s18/l92/l92.js
+++ s18/l92/l92.js
@@ -4,5 +4,25 @@
 
 // execute callback when the DOM is loaded
 document.addEventListener('DOMContentLoaded', function(){
+    // ENSURE THE DOM IS READY
+    var doc = document;
+    var f = doc.querySelector('#form');
+    
+    // FIRST WAY TO HANDLE A SUBMIT
+    // f.onsubmit = (e) => {
+    //     // TEST COMMANDS
+    //     // console.log(e);
+    //     // console.log('Form f: submitted!');
+    //     // return false;
 
+    //     // PREVENT THE DEFAULT BEHAVIOUR
+    //     e.preventDefault();
+    //     alert(e.target.name.value);
+    // }
+
+    // SECOND WAY TO HANDLE A SUBMIT
+    f.addEventListener('submit', () => {
+        alert('f submitted!');
+        return false;
+    });
 });
