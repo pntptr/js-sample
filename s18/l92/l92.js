11aa5023a8fb0e754a3f89f105cf30eb4aa0aff7
pietro <pietro.pnt@gmail.com>
2019-02-16 16:04:53 +0100

    aggiunta lezione 92

---
+++ s18/l92/l92.js
@@ -0,0 +1,8 @@
+// SEZIONE 18, LEZIONE 92 - CORSO UDEMY JS
+// GLI EVENTI IN JAVASCRIPT
+// 
+
+// execute callback when the DOM is loaded
+document.addEventListener('DOMContentLoaded', function(){
+
+});
