3e0f0c1b3ea95d7598aa12db17150f6d0ef0290a
pntptr <pietro.pnt@gmail.com>
2019-05-01 16:34:30 +0200

    aggiunti modelli html

---
+++ s21/l138/l138.js
@@ -0,0 +1,10 @@
+// SEZIONE 21, LEZIONE 138 - CORSO UDEMY JS
+// JAVASCRIPT ES6: PROMISE
+// Javascript ES6: Promise: resolve, reject, then e catch
+
+/*
+    Promise state: 
+    - Pending
+    - Fullfilled (resolve) 
+    - Rejected (reject)
+*/
